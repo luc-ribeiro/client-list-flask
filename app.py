0201fdf664bc3ebd4c8c80a02f1b74b9e7fd41f6
Lucas Ribeiro <lucasribeirodesouza@live.com>
2021-11-25 08:52:30 -0300

    Minor fixes

--- app.py
+++ app.py
@@ -6,28 +6,36 @@ clients = []
 clients_search = []
 
 # HOME #
+
+
 @app.route('/')
 def index():
     return render_template('index.html', list_clients=clients, list_clients_search=clients_search)
 
 # SAVE #
+
+
 @app.route('/save-client', methods=['POST'])
 def save():
     end_search()
 
-    if not clients: id = 1 
-    else: id = clients[-1]['id'] + 1
+    if not clients:
+        id = 1
+    else:
+        id = clients[-1]['id'] + 1
 
     name = request.form['name']
     phone = request.form['phone']
     subject = request.form['subject']
 
-    client = { "id": id, "name": name, "phone": phone, "subject": subject }
+    client = {"id": id, "name": name, "phone": phone, "subject": subject}
     clients.append(client)
 
     return redirect('/')
 
 # DELETE #
+
+
 @app.route('/delete-client/<int:id>')
 def delete(id):
     end_search()
@@ -40,24 +48,33 @@ def delete(id):
     return redirect('/')
 
 # SEARCH #
+
+
 @app.route('/search-client', methods=['POST'])
 def search():
     end_search()
 
     query = request.form['search-client'].lower()
-    if query == '': return redirect('/')
-    else: 
+
+    if query == '':
+        return redirect('/')
+    else:
         for i in range(len(clients)):
-            if (query in clients[i]['name'].lower()) or (query in clients[i]['phone'].lower()) or (query in clients[i]['subject'].lower()):
+            if query in clients[i]['name'].lower() or \
+                    query in clients[i]['phone'].lower() or \
+                    query in clients[i]['subject'].lower():
                 clients_search.append(clients[i])
 
     return redirect('/')
 
-# END SEARCH #   
+# END SEARCH #
+
+
 @app.route('/search-client/end')
 def end_search():
     clients_search.clear()
 
     return redirect('/')
+
 
 app.run(debug=True)
